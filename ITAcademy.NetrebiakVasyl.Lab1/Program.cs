da4074a47684649726d86dff0db51c05aad45825
Netrebiak Vasyl <vnetreby@gmail.com>
2020-10-03 17:01:23 +0300

    Adding Lab1 and Lab2

---
+++ ITAcademy.NetrebiakVasyl.Lab1/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+namespace ITAcademy.NetrebiakVasyl.Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lab1");
+            Console.Write("Input value of N: ");
+
+
+            long N;
+            N = Convert.ToInt64(Console.ReadLine());
+
+
+            int DigitsInNumber;
+            DigitsInNumber = AmtOfNumbers(N);
+            Console.WriteLine("Amount of numbers in N = " + DigitsInNumber);
+            Console.WriteLine();
+
+
+            const int SizeOfArray = 20;//Size 20 because of max value of 'long'
+            long[] MassiveOfNumbers = new long[SizeOfArray];
+            OrganizeMassive(ref MassiveOfNumbers, N);
+            for (int i = 0; i < DigitsInNumber; i++)
+            {
+                Console.WriteLine($"MassiveOfNumbers[{i}]={MassiveOfNumbers[i]}");
+            }
+            Console.WriteLine();
+
+
+            double ArithmeticMeanOfNNums=ArithmeticMean(N, DigitsInNumber);
+            Console.WriteLine($"Arithmetic mean of digits of N = {ArithmeticMeanOfNNums}");
+            Console.WriteLine();
+
+
+            double GeometricMeanOfNNums=GeometricMean(N, DigitsInNumber);
+            Console.WriteLine($"Geometric mean of digits of N = {GeometricMeanOfNNums}");
+            Console.WriteLine();
+
+            double FactorialOfNum = FactorialRecursion(N);
+            Console.WriteLine($"Factorial of N with recursion = {FactorialOfNum}");
+            FactorialOfNum = FactorialLoop(N);
+            Console.WriteLine($"Factorial of N with Loop = {FactorialOfNum}");
+            Console.WriteLine();
+
+            long SumOfEvenNumbersToNum;
+            SumOfEvenNumbersToNum = SumOfEvenNumbers_ForLoop(N);
+            Console.WriteLine($"Sum of even numbers from 1 to N using FOR loop = {SumOfEvenNumbersToNum}");
+            SumOfEvenNumbersToNum = SumOfEvenNumbers_WhileLoop(N);
+            Console.WriteLine($"Sum of even numbers from 1 to N using WHILE loop = {SumOfEvenNumbersToNum}");
+            SumOfEvenNumbersToNum = SumOfEvenNumbers_DoWhileLoop(N);
+            Console.WriteLine($"Sum of even numbers from 1 to N using DO WHILE loop = {SumOfEvenNumbersToNum}");
+            Console.WriteLine();
+
+            long SumOfOddNumbersToNum;
+            SumOfOddNumbersToNum = SumOfOddNumbers(N);
+            Console.WriteLine($"Sum of odd numbers from 1 to N = {SumOfOddNumbersToNum}");
+
+            long SumOfEvenNums;
+            long SumOfOddNums;
+            Console.Write("Enter first num of range: ");
+            int FirsNumOfRange = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter end num of range: ");
+            int SecondNumOfRange=Convert.ToInt32(Console.ReadLine());
+            SumOfEvenNums = SumOfEvenNumbers(FirsNumOfRange, SecondNumOfRange);
+            SumOfOddNums = SumOfOddNumbers(FirsNumOfRange, SecondNumOfRange);
+            Console.WriteLine($"Sum of even numbers in range={SumOfEvenNums}, sum of odd={SumOfOddNums}");
+
+            Console.ReadKey();
+        }
+
+        static int AmtOfNumbers(long YourNumber)//Amt.==Amount
+        {
+            int CounterOfNumbers = 0;
+            while (YourNumber > 0)
+            {
+                YourNumber = YourNumber / 10;
+                CounterOfNumbers++;
+            }
+            return CounterOfNumbers;
+        }
+        static void OrganizeMassive(ref long[] massive, long YourNumber)
+        {
+            int IndexOfMassive = 0;
+            while (YourNumber > 0)
+            {
+                massive[IndexOfMassive] = YourNumber % 10;
+                YourNumber /= 10;
+                IndexOfMassive++;
+            }
+        }
+
+        static double ArithmeticMean(long YourNumber, int SumOfNumbers)
+        {
+            double result=0;
+            double temp = 0;
+            while (YourNumber > 0)
+            {
+                temp = YourNumber % 10;
+                result += temp;
+                YourNumber /= 10;
+            }
+            result /= SumOfNumbers;
+            return result;
+        }
+
+        static double GeometricMean(long YourNumber, int SumOfNumbers)
+        {
+            double result = 1;
+            double temp = 0;
+            while (YourNumber > 0)
+            {
+                temp = YourNumber % 10;
+                result *= temp;
+                YourNumber /= 10;
+            }
+            return Math.Pow(result, 1.0/SumOfNumbers);
+        }
+        static double FactorialRecursion(long YourNumber)
+        {
+            if (YourNumber == 1)
+                return 1;
+            else
+                return YourNumber * FactorialRecursion(YourNumber - 1);
+        }
+        static double FactorialLoop(long YourNumber)
+        {
+            double result = 1;
+            while (YourNumber != 1)
+            {
+                result = result * YourNumber;
+                YourNumber = YourNumber - 1;
+            }
+            return result;
+        }
+        static long SumOfEvenNumbers_ForLoop(long YourNumber)
+        {
+            long sum = 0;
+            for(int i = 0; i < YourNumber; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+        static long SumOfEvenNumbers_WhileLoop(long YourNumber)
+        {
+            long sum = 0;
+            int i=0;
+            while (i < YourNumber)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+                i++;
+            }
+            return sum;
+        }
+        static long SumOfEvenNumbers_DoWhileLoop(long YourNumber)
+        {
+            long sum = 0;
+            int i = 0;
+            do
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+                i++;
+            }
+            while (i < YourNumber);
+            return sum;
+        }
+        static long SumOfOddNumbers(long YourNumber)
+        {
+            long sum = 0;
+            for (int i = 0; i < YourNumber; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+        static long SumOfOddNumbers(int From, int To)
+        {
+            long sum = 0;
+            for (int i = From; i < To; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+        static long SumOfEvenNumbers(int From, int To)
+        {
+            long sum = 0;
+            for (int i = From; i < To; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ ITAcademy.NetrebiakVasyl.Lab2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace ITAcademy.NetrebiakVasyl.Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter text to first string: ");
+            StringBuilder Str1=new StringBuilder(Console.ReadLine());
+
+            Console.Write("Enter text to second string: ");
+            StringBuilder Str2=new StringBuilder(Console.ReadLine());
+
+            bool result = ContainsAllSymbols(Str1, Str2);
+            Console.WriteLine($"Does first string contain all symbols of second string : {result}");
+
+            StringBuilder Sharped=new StringBuilder();
+            Sharped.Append(Str1);
+            Sharped=ReplaceWithSharpEqualSymbols(Sharped, Str2);
+            Console.WriteLine($"Similar symbols replaced with # : {Sharped} ");
+
+            int CounterOfStrInStr = CountStrInStr(Str1, Str2);
+            Console.WriteLine($"Str2 in Str1 : {CounterOfStrInStr}");
+
+            Console.ReadKey();
+        }
+        static bool ContainsAllSymbols(StringBuilder Text, StringBuilder Subtext)
+        {
+            int counter=0;
+            for(int i = 0; i < Subtext.Length; i++)
+            {
+                for(int j = 0; j < Text.Length; j++)
+                {
+                    if (Subtext[i] == Text[j])
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+            }
+            if (counter  == Subtext.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        static StringBuilder ReplaceWithSharpEqualSymbols(StringBuilder Text, StringBuilder Subtext)
+        {
+            for (int i = 0; i < Subtext.Length; i++)
+            {
+                for (int j = 0; j < Text.Length; j++)
+                {
+                    if (Subtext[i] == Text[j])
+                    {
+                        Text[j] = '#';
+                    }
+                }
+            }
+            return Text;
+        }
+
+        static int CountStrInStr(StringBuilder Text, StringBuilder Subtext)
+        {
+            int counter = 0;
+            int i = 0;
+            int j = 0;
+            int temp = 0;
+            while (i < Text.Length)
+            {
+                if (Text[i] != Subtext[j])
+                {
+                    i++;
+                    j=0;
+                    temp = 0;
+                    continue;
+                }
+                if (Text[i] == Subtext[j])
+                {
+                    temp++;
+                }
+                if (temp == Subtext.Length)
+                {
+                    i++;
+                    j = 0;
+                    temp = 0;
+                    counter++;
+                    continue;
+                }
+                i++;
+                j++;
+            }
+            return counter;
+        }
+    }
+}
